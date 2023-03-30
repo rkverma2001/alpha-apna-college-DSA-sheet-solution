01d21bc98f09a963076855058eed179c40aed813
Ritesh Kumar <riteshkumarverma2018@gmail.com>
2023-03-30 07:02:54 +0530

    binary search for sorted arrays

--- binary_search.java
+++ binary_search.java
@@ -1,22 +1,27 @@
+// Only for sorted arrays.
 public class binary_search {
     public static int binarySearch(int number[], int key) {
         int start = 0;
-        int end = number.length-1;
-        while(start<=end) {
-            int mid = (start+end)/2;
-            if(number[mid] == key) {
+        int end = number.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // comparisons
+            if (number[mid] == key) {
                 return mid;
             }
-            if(number[mid] < key) {
+            if (number[mid] < key) {
                 start = mid + 1;
             } else {
-                end = mid -1;
+                end = mid - 1;
             }
         }
         return -1;
     }
+
     public static void main(String[] args) {
-        int arr[] = {2, 5, 1, 8, 7, 6};
-        System.out.println(binarySearch(arr, 6));
+        int arr[] = { 2, 4, 6, 8, 10, 12, 14 };
+        int key = 10;
+        System.out.println(binarySearch(arr, key));
     }
 }
